ea43d440ce885cfc22148b41d4d9ad418048757e
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-06-21 09:37:48 +0700

    เพิ่ม index+main

---
+++ main.js
@@ -0,0 +1,7 @@
+let x = 15;
+let y = 8;
+let z = x + y;
+
+document.getElementById("test").innerHTML= y + y;
+document.getElementById("test2").innerHTML = x *10;
+document.getElementById("test3").innerHTML = z;
