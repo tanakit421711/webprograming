d62453340ce76aabc485d324600bd4408b99e4a5
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-06-21 10:54:09 +0700

    การเขียน arrow Fun

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
-// Jakkrit Sukajan
-/*
-week 2 - javascript
-*/
-let name = "Jakkrit"
-let lastname;
-lastname = "Sukajan"
+/*function sum (x,y){
+    return x+y;
 
-//console.log(`สวัสดีคุณ ${name} ${lastname}`)
-
-const PI=3.14;
-
-let obj = {
-    name: "Jakkrit",
-    lastname: "Sukajan",
-    address: {
-        number: "7/2",
-        province: "พิษณุโลก"
-    }
 }
+*/
 
-console.log(obj.name);
-console.log(obj.lastname);
-console.log(obj.address.number);
-console.log(obj.address.province);
+const sum = (x,y) => x+y;
+const minus = (x,y) => x-y;
+const multi = (x,y) => x*y;
+const div = (x,y) => x/y;
+
+let x = sum (5,10);
+let y = minus(40,30);
+let z = multi(8,5);
+let i = div(100,2);
+
+sum(1,2)
+sum(2,4)
+sum(4,6)
+sum(6,8)
+
+console.log(`x = ${x}`);
+console.log(`y = ${y}`);
+console.log(`z = ${z}`);
+console.log(`i = ${i}`);
+
+
+
+/*
+function sum(x,y) {
+    console.log("Show Sum");
+    console.log(x+y);
+}
+*/
+
+/*
+const sum = (x,y)=> {
+    console.log("Show Sum");
+    console.log(x+y);
+}
+*/
+
