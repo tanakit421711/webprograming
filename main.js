eb93dfcbfbff1c9d81f6b0ae5f2e0dbc8fe05918
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-06-21 11:05:02 +0700

    ขอบเขตตัวแปร

--- main.js
+++ main.js
@@ -1,42 +1,10 @@
-/*function sum (x,y){
-    return x+y;
+let x = 10;
 
+function show (){
+    let x =20;
+    return x;
 }
-*/
 
-const sum = (x,y) => x+y;
-const minus = (x,y) => x-y;
-const multi = (x,y) => x*y;
-const div = (x,y) => x/y;
-
-let x = sum (5,10);
-let y = minus(40,30);
-let z = multi(8,5);
-let i = div(100,2);
-
-sum(1,2)
-sum(2,4)
-sum(4,6)
-sum(6,8)
-
-console.log(`x = ${x}`);
-console.log(`y = ${y}`);
-console.log(`z = ${z}`);
-console.log(`i = ${i}`);
-
-
-
-/*
-function sum(x,y) {
-    console.log("Show Sum");
-    console.log(x+y);
-}
-*/
-
-/*
-const sum = (x,y)=> {
-    console.log("Show Sum");
-    console.log(x+y);
-}
-*/
-
+console.log(x);
+console.log(show());
+console.log(x);
