fdd479aa0d8d8d78462c24cb24afab022aab778a
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-06-21 10:19:51 +0700

    ตัวแปรแบบต่างๆ

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-let show = "RMUTL"
-let name = "Jakkrit Sukajan"
+// Jakkrit Sukajan
+/*
+week 2 - javascript
+*/
+let name = "Jakkrit"
+let lastname;
+lastname = "Sukajan"
 
-console.log("Javascript")
-console.log(show)
-console.log(name)
+//console.log(`สวัสดีคุณ ${name} ${lastname}`)
 
-//document.getElementById("show").innerHTML= show;
+const PI=3.14;
+
+let obj = {
+    name: "Jakkrit",
+    lastname: "Sukajan",
+    address: {
+        number: "7/2",
+        province: "พิษณุโลก"
+    }
+}
+
+console.log(obj.name);
+console.log(obj.lastname);
+console.log(obj.address.number);
+console.log(obj.address.province);
