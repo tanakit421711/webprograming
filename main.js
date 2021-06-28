3199e3a8f924b40dad8c695ceb9ee81bfff697b0
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-06-28 10:28:28 +0700

    package

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-let x = 10;
-
-function show (){
-    let x =20;
-    return x;
+//Destructuring
+let obj = {
+    name: "Jakkrit",
+    lastname: "Sukajan",
+    age: 20,
+    salary: 0,
+} 
+let obj2 = {
+    name: "Worachit",
+    lastname: "Toto",
+    age: 20,
+    salary: 100,
 }
+const showdata =({name,lastname,age,salary}) =>`สวัสดีคุณ ${name} ${lastname}
+ขณะนี้คุณมีอายุเท่ากับ ${age}
+ขณะนี้คุณมีเงินเดือนเท่ากับ ${salary}`;
 
-console.log(x);
-console.log(show());
-console.log(x);
+
+
+console.log(showdata(obj));
+console.log(showdata(obj2));
+
+// Spread Operator
+
+const fun = (x,y,z) => x+y+z;
+
+let num = [5,6,7];
+
+let res = fun(...num);
+
+console.log(res);
