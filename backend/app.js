37b93659a2f3e0ba048702da7bb4071eead0918d
Jakkrit Sukajan <garfieldza34@gmail.com>
2021-07-12 08:47:46 +0700

    create backend

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const mongoose = require("mongoose");
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+// mongo
+const usersRouter_mongo = require("./routes/mongo/users");
+
 const app = express();
+
+mongoose.connect("mongodb+srv://admin:gf0877374410@cluster0.dz5xb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// mongo
+app.use("/mongo", usersRouter_mongo);
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/api', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('RMUTL TEST API');
 });
 
 module.exports = router;
